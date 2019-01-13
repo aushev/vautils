6335b7d645084078843e1b3ba16ab7ac4f60720c
Vasily Aushev <vaushev@gmail.com>
2019-01-13 05:32:22 -0500

    added tryRdat function

--- R/data.R
+++ R/data.R
@@ -262,3 +262,28 @@ orderby <- function(x,y){
 }
 
 na.allow <- function(input) return(is.na(input) | input);
+
+
+tryRdat <- function(fnRdat, FUN, ...){
+# This function tries to load data from the indicated Rdat file
+# if it doesn't exist, it will call indicated function, and save the results to the file
+# Function must return named objects in a list
+  cat('Checking pre-existing Rdat file:', fnRdat, '... ');
+  if (file.exists(fnRdat)) {
+    cat(' found. ');
+    load(fnRdat, verbose = T, envir = parent.frame(n=2));
+  } else {
+    cat(' not found. Will be generated and saved.\n');
+    result <- do.call(FUN, args = list(...))
+    
+    names_to_save <- c()
+    for(this_name in names(result)){
+      assign(value = result[[this_name]], x=this_name) # , envir = parent.frame()
+      names_to_save <- c(names_to_save,this_name)
+    }
+    
+    save(list = names_to_save, file = fnRdat);
+    load(fnRdat, verbose = T, envir = parent.frame(n=2));
+  } # e. else
+
+} # e. try_rdat
