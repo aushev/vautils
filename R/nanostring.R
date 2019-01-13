464b781ca12ef8874fbe91aa921805665f8c783d
Vasily Aushev <vaushev@gmail.com>
2019-01-13 06:12:12 -0500

    error in apply.PCF()

--- R/nanostring.R
+++ R/nanostring.R
@@ -1,8 +1,9 @@
 
 nses.ini1 <- function(es){
-  
+
   fData(es)$CodeClass[fData(es)$CodeClass=='Endogenous1'] <- 'Endogenous'
-  
+
+  # should be done AFTER applying PCF!
   sum.pos <- colSums(exprs(es[fData(es)$CodeClass == 'Positive',]))
   sum.neg <- colSums(exprs(es[fData(es)$CodeClass == 'Negative',]))
   sum.hkg <- colSums(exprs(es[fData(es)$CodeClass == 'Housekeeping',]))
@@ -13,22 +14,6 @@ nses.ini1 <- function(es){
   es %<>% attachpData(dat.add)
 }
 
-extract.PCF <- function(es, label.GeneName='GeneName', label.PCF='PCF'){
-  # extract & remove PCF from GeneName column of fData, save it to new 'PCF' column of fData 
-  # debug: es=es.raw; label.GeneName='GeneName'; label.PCF='PCF'
-  # end debug: rm(es,label.GeneName,label.PCF,geneNames,have.PCF,PCFs)
-  geneNames <- fData(es)[[label.GeneName]]; # ex: 'hsa-miR-302d-3p|0.006', or 'LIG_POS_B'
-  have.PCF <- grepl('\\|\\d',geneNames)
-  if (sum(have.PCF)==0) {
-    warning('PCF not found in gene names. Returning unchanged.');
-    invisible(es);
-  }
-  geneNames[!have.PCF] <- paste0(geneNames[!have.PCF], '|0'); 
-  PCFs <- as.numeric(            gsub(   '.*\\|(\\d+)','\\1',geneNames))
-  fData(es)[[label.GeneName]] <- gsub('^(.+)\\|(.*)'  ,'\\1',geneNames)
-  fData(es)[[label.PCF]] <- PCFs
-  invisible(es);
-}
 
 nses.spike <- function(es){
     # very dirty
@@ -49,6 +34,23 @@ nses.spike <- function(es){
   invisible(es);
 }
 
+extract.PCF <- function(es, label.GeneName='GeneName', label.PCF='PCF'){
+  # extract & remove PCF from GeneName column of fData, save it to new 'PCF' column of fData
+  # actual expression values will not be changed yet; use apply.PCF()
+  # debug: es=es.raw; label.GeneName='GeneName'; label.PCF='PCF'
+  # end debug: rm(es,label.GeneName,label.PCF,geneNames,have.PCF,PCFs)
+  geneNames <- fData(es)[[label.GeneName]]; # ex: 'hsa-miR-302d-3p|0.006', or 'LIG_POS_B'
+  have.PCF <- grepl('\\|\\d',geneNames)
+  if (sum(have.PCF)==0) {
+    warning('PCF not found in gene names. Returning unchanged.');
+    invisible(es);
+  }
+  geneNames[!have.PCF] <- paste0(geneNames[!have.PCF], '|0');
+  PCFs <- as.numeric(            gsub(   '.*\\|(\\d+)','\\1',geneNames))
+  fData(es)[[label.GeneName]] <- gsub('^(.+)\\|(.*)'  ,'\\1',geneNames)
+  fData(es)[[label.PCF]] <- PCFs
+  invisible(es);
+}
 
 
 apply.PCF <- function(es, extract=TRUE){
@@ -57,7 +59,7 @@ apply.PCF <- function(es, extract=TRUE){
   PCFs <- fData(es)$PCF;
   if (is.null(PCFs) && extract!=TRUE) {stop('Need to extract PCF first!');}
   if (is.null(PCFs)) {es %<>% extract.PCF; }
-  posA <- exprs(es)[fData(es.raw)$GeneName=='POS_A']
+  posA <- exprs(es)[fData(es)$GeneName=='POS_A']
   backgrounds <- sapply(posA, FUN = '*',PCFs)
   X <- exprs(es) - backgrounds
   X[X<0] <- 0
