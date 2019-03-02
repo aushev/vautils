03e0275e46467f82ed43791b49ee01eae4a37d26
Vasily Aushev <vaushev@gmail.com>
2019-03-02 00:57:24 -0500

    eSet.R: attachpData(), attachfData()

--- R/eSet.R
+++ R/eSet.R
@@ -1,36 +1,29 @@
 
 # extends phenoData of given eSet by adding a data.table
-attachpData <- function(es, dat, by.dat=NULL, by.es=NULL, reqUniqESkeys=T){
-
-  # if no fields indicated, then add complete [dat] to pData
-  if (is.null(by.es) & is.null(by.dat)) {
-     pData(es) <-  cbind(pData(es), dat);
-     return(invisible(es));
-  } # return & exit
+attachpData <- function(es, dat, key.dat=NULL, key.es=NULL, reqUniqESkeys=T){
 
   if (!is.data.table(dat)) dat <- data.table(dat);
+  dat.use <- dat;
+  if (is.null(key.dat) & !is.null(key.es)) warning('Key of dat is not defined => Key of es will be ignored!');
 
-  #
-  if (!is.null(by.dat)){
-    keyvals <- dat[[by.dat]];
+  if (!is.null(key.dat)){
+    keyvals <- dat[[key.dat]];
     if (anyDuplicated(keyvals)) {
       print(keyvals[duplicated(keyvals)]);
       stop('Duplicated key values found in dat!');
     }
+    setkeyv(dat, key.dat);
 
-    dat <- data.table(dat);
-    setkeyv(dat, by.dat);
+    # if key.es is undefined, we use sampleNames(es), otherwise defined key.es col
+    es.keys <- ifelse1(is.null(key.es), sampleNames(es), pData(es)[[key.es]])
+    if (anyDuplicated(es.keys) & reqUniqESkeys) {
+      print(es.keys[duplicated(es.keys)])
+      warning('Duplicated es.keys found! Some of the dat records will be duplicated.');
+    }
+    dat.use <- dat[es.keys,]; # only selected records from dat
+    dat.use <- dat.use[,-c(key.dat),with=F]; # #dat.use <- dat.use[,(names(dat.use) %-% key.dat),with=F]; #
+
   }
-
-  # if key.col is defined only for dat, we use sampleNames(es), otherwise defined by.es col
-  es.keys <- ifelse1(is.null(by.es), sampleNames(es), pData(es)[[by.es]])
-  if (anyDuplicated(es.keys) & reqUniqESkeys) {
-    print(es.keys[duplicated(es.keys)])
-    warning('Duplicated es.keys found!');
-  }
-
-  dat.use <- dat[es.keys,];
-  dat.use <- dat.use[,-c(by.dat),with=F]; # #dat.use <- dat.use[,(names(dat.use) %-% by.dat),with=F]; #
 
   names.dup <- colnames(dat.use) %&% varLabels(es); # colnames() instead of ... in case of matrix
   if (length(names.dup)>0) {
@@ -42,53 +35,39 @@ attachpData <- function(es, dat, by.dat=NULL, by.es=NULL, reqUniqESkeys=T){
   invisible(es);
 }
 
+# extends featureData of given eSet by adding a data.table
+attachfData <- function(es, dat, key.dat=NULL, key.es=NULL, reqUniqESkeys=T){
 
+  if (!is.data.table(dat)) dat <- data.table(dat);
+  dat.use <- dat;
+  if (is.null(key.dat) & !is.null(key.es)) warning('Key of dat is not defined => Key of es will be ignored!');
 
+  if (!is.null(key.dat)){
+    keyvals <- dat[[key.dat]];
+    if (anyDuplicated(keyvals)) {
+      print(keyvals[duplicated(keyvals)]);
+      stop('Duplicated key values found in dat!');
+    }
+    setkeyv(dat, key.dat);
 
+    # if key.es is undefined, we use featureNames(es), otherwise defined key.es col
+    es.keys <- ifelse1(is.null(key.es), featureNames(es), fData(es)[[key.es]])
+    if (anyDuplicated(es.keys) & reqUniqESkeys) {
+      print(es.keys[duplicated(es.keys)])
+      warning('Duplicated es.keys found! Some of the dat records will be duplicated.');
+    }
+    dat.use <- dat[es.keys,]; # only selected records from dat
+    dat.use <- dat.use[,-c(key.dat),with=F]; # #dat.use <- dat.use[,(names(dat.use) %-% key.dat),with=F]; #
 
-
-
-
-
-
-
-
-
-attachpData0 <- function(es, dat, keycol, es.keycol=NULL){
-  #if (!is.data.table(dat)) dat <- data.table(dat);
-
-  if (missing(keycol)){
-    keyvals0 <- row.names(dat);
-  } else {
-    keyvals0 <- dat[[keycol]]
-    setkeyv(dat,keycol);
   }
 
-  es.keys <- ifelse1(is.null(es.keycol), sampleNames(es), pData(es)[[es.keycol]])
-  if (anyDuplicated(es.keys)) {
-    print(es.keys[duplicated(es.keys)])
-    stop('Duplicated es.keys found!');
+  names.dup <- colnames(dat.use) %&% fvarLabels(es); # colnames() instead of ... in case of matrix
+  if (length(names.dup)>0) {
+    fData(es) <- fData(es)[,(fvarLabels(es) %-% names.dup)]
+    warning('Replaced columns in phenoData: ', paste(names.dup, collapse = ', '))
   }
 
-  names.nof <- es.keys %-% keyvals0;
-  if (length(names.nof)>0)
-    warning('phenoData IDs not found in key: ', paste(names.nof, collapse = ', '))
-
-  dat.use <- dat[es.keys,];
-
-  if (missing(keycol)){
-    keyvals <- row.names(dat.use);
-  } else {
-    keyvals <- row.names(dat.use) <-  dat.use[[keycol]];
-    dat.use <- dat.use[,-c(keycol),with=F];
-  }
-
-  if (anyDuplicated(keyvals)) stop('Duplicated key values found!');
-
-  names.dup <- colnames(dat.use) %&% varLabels(es); # colnames() instead of colnames() in case of matrix
-  if (length(names.dup)>0) stop('Columns already in phenoData: ', paste(names.dup, collapse = ', '))
-
-  pData(es) <-  cbind(pData(es), dat.use);
+  fData(es) <-  fbind(fData(es), dat.use);
   invisible(es);
 }
 
