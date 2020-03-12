ea7ff1c50cc6bd700e2995087f2988beee8ca5c0
unknown <vaushev@gmail.com>
2020-03-11 22:15:11 -0400

    selectionView()

--- R/rstudio_addins.R
+++ R/rstudio_addins.R
@@ -4,7 +4,19 @@
 selectionView <- function(){
   context <- rstudioapi::getActiveDocumentContext()
   sel_text <- context$selection[[1]]$text
+  sel_obj <- get(sel_text)
   # print(sel_text)
-  View(get(sel_text), title=sel_text)
+
+  if ('ExpressionSet' %in% class(sel_obj)){
+    tmpX <- exprs(sel_obj)
+    tmpF <- fData(sel_obj)
+    tmpP <- pData(sel_obj)
+
+    View(tmpX, title=paste0(sel_text,'$X'))
+    View(tmpF, title=paste0(sel_text,'$F'))
+    View(tmpP, title=paste0(sel_text,'$P'))
+  } else {
+    View(sel_obj, title=sel_text)
+  }
 }
 
